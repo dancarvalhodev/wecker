a7dc289dbe791c2e3109fb9fa8563d7d64103871
Daniel Carvalho <danielcarvalhodev@gmail.com>
2026-02-04 20:26:44 -0300

    :sparkles: Initial deps commit and structure configuration.

--- www/src/Main.php
+++ www/src/Main.php
@@ -2,15 +2,73 @@
 
 namespace App;
 
+use DI\Container;
+use DI\ContainerBuilder;
+use Dotenv\Dotenv;
+use Exception;
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+# IMPROVE LEARNING ABOUT PHP DI AND HOW WORKS
 final class Main
 {
+    private Container $container;
+
+    /**
+     * @var App
+     */
+    private App $app;
+
+    /**
+     * @throws Exception
+     */
     public function __construct()
     {
-
+        $this->loadConfigs();
+        $this->buildContainer();
+        $this->createApp();
     }
 
-    public function run()
+    /**
+     * @return void
+     */
+    public function run(): void
     {
+        # HERE ENTER ROUTE SYSTEM
+        $this->app->get('/', function (Request $request, Response $response, $args) {
+            $response->getBody()->write("Hello world!");
+            return $response;
+        });
 
+        $this->app->run();
+    }
+
+    /**
+     * @return void
+     */
+    private function loadConfigs(): void
+    {
+        Dotenv::createImmutable(__DIR__ . '/config')->safeLoad();
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    private function buildContainer(): void
+    {
+        $builder = new Container();
+        $this->container = $builder;
+    }
+
+    /**
+     * @return void
+     */
+    private function createApp(): void
+    {
+        AppFactory::setContainer($this->container);
+        $this->app = AppFactory::create();
     }
 }
