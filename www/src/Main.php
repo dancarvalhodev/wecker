0c1fee08b34749c08dfa81fbaee04613656c2108
Daniel Carvalho <danielcarvalhodev@gmail.com>
2026-02-04 20:11:11 -0300

    :sparkles: Initial deps commit and structure configuration.

---
+++ www/src/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+final class Main
+{
+    public function __construct()
+    {
+
+    }
+
+    public function run()
+    {
+
+    }
+}
---
+++ www/src/public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Main;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+(new Main())->run();
